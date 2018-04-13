3a6feddb76ca78f6bab6ec335f620127c28008d7
observer <vasyarus@inbox.ru>
2018-04-13 17:55:50 +0300

    13.04.2018 urls.py edit

--- diary/urls.py
+++ diary/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'current', views.current_tasklist, name='active'),
-    url(r'complete', views.complete_taslist, name='done'),
+    url(r'^current', views.current_tasklist, name='active'),
+    url(r'^complete', views.complete_taslist, name='done'),
+    url(r'^$', RedirectView.as_view(url='current'))
 ]
